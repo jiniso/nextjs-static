3fdba486eaa9f2fce52a565ff03e50075093bbc7
jiniso <joseph.jager@gmail.com>
2019-05-25 10:19:58 -0700

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,6 +1,5 @@
 export default () => (
   <header>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140858165-1"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
