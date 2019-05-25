2dd6f8fd81870b25e8509337a14bf6e449454d65
jiniso <joseph.jager@gmail.com>
2019-05-25 11:42:09 -0700

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,14 +1,5 @@
 export default () => (
   <header>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140858165-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-140858165-1');
-    </script>
-
     <h1>Static Next.js Example on Now 2.0</h1>
   </header>
 )
